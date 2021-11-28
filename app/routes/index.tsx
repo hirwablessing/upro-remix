bc70b409be87ce4a9bd12d93efcdcdb4c08c69eb
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-11-28 22:00:38 +0200

    chore: initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,20 @@
+import type {MetaFunction} from "remix";
+
+
+
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix Starter",
+    description: "Welcome to remix!"
+  };
+};
+
+export default function Index() {
+
+  return (
+    <div className="grid place-items-center h-screen w-screen bg-black text-white">
+      <h1>Remix Starter and tailwindcss</h1>
+    </div>
+  );
+}
