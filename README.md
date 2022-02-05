9ed2820dc77487c8cc187d13721d2032dd7a1916
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-02-05 15:37:45 +0200

    chore: fixed postcss dependancy bug

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-## upro-remix
+## upro
 
 Starter template for remix applications
+
+#### Getting started
+
+```bash
+pnpm dlx degit upro-remix veritem/upro-remix <project-name>
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['./app/**/*.tsx'],
+    content: ['./app/**/*.{ts,tsx,jsx,js}'],
     theme: {
         fontFamily: {
             primary: ['Inter', 'sans-serif'],
