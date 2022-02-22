d9fdd373aea2e858cecb8c724596b22dd5cb0052
Blessing Hirwa <50826640+hirwablessing@users.noreply.github.com>
2022-02-22 09:07:33 +0200

    fix: path to tailwind css file
    The path was `"./tailwind.css"` while it should be `"./styles/tailwind.css"`

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, useCatch } from 'remix'
-import styles from './tailwind.css'
+import styles from './styles/tailwind.css'
 
 export function links() {
     return [{ rel: 'stylesheet', href: styles }]
